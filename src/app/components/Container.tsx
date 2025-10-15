4fe1309776ff865a954fd68ed6576f3dfe21c29c
Shakiba Vakili <shakibvakili@gmail.com>
2025-10-15 11:25:55 +0330

    add gradient background to Container component for consistency

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function Container({children}:{children: React.ReactNode}) {
+function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className='container mx-auto'>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+      <div className="container mx-auto px-6 py-10">
         {children}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
